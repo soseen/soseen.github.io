738aecba58eb20a12f5a181f318873200cb2fe9f
Przemek Sosnowski <lampard082@gmail.com>
2019-05-11 09:55:50 +0200

    jquery selectors

---
+++ jquery/cwiczenia/js/2-selector.js
@@ -0,0 +1,11 @@
+var naglowek1 = $("#naglowek-1");
+console.log(naglowek1);
+
+naglowek1.css('color', 'red');
+
+var paragrafy = $('p');
+paragrafy.css('color', 'blue');
+
+var paragrafyKlasy = $('.par');
+paragrafyKlasy.css({color: 'green'});
+console.log();
