f7cb6089fc0f3e2d1f1a1b2d11aa315c5b8861a4
Przemek Sosnowski <sosnowski.przemek.95@gmail.com>
2019-05-14 01:48:53 +0200

    ajax-zadanie

--- AJAX/1-pobierz-dane-programisty/js/main.js
+++ AJAX/1-pobierz-dane-programisty/js/main.js
@@ -2,13 +2,24 @@ var btn = $('#btn-1');
 
 btn.click(function(){
 
-    var container = $('<div></div<');
+    var container = $('<div></div<').attr('id', 'dane-programisty');
+    container.css({
+        'background-color': 'bisque',
+        'width': '10%',
+        'border': '1px solid black',
+        'margin-top': '2px'
+    });
     $.getJSON
     ('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php',
         response => {
             $.each(response ,function(key, value){
                 console.log(key, value);
-                container.append(`<p>${key}: ${value}</p>`);
+                var paragraph = $('<p></p>').text(`${key}: ${value}`);
+                paragraph.css({
+                    'margin-left': '2px',
+                    'font-family': 'Tahoma, Geneva, sans-serif'
+                });
+                container.append(paragraph);
                 });
             $('body').append(container);
             }
